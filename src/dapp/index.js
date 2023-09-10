77f7331d1899442ba50e0bfab317fb48fb451b73
brmnl <50099074+brmnl@users.noreply.github.com>
2023-09-10 16:45:35 +0200

    Updating UI design

--- src/dapp/index.js
+++ src/dapp/index.js
@@ -38,8 +38,6 @@ function display(title, description, results) {
   section.appendChild(DOM.h5(description));
   results.map((result) => {
     let row = section.appendChild(DOM.div({ className: "row mt-4" }));
-    // row.appendChild(DOM.div({className: 'col-sm-4 field'}, result.label));
-    // row.appendChild(DOM.div({className: 'col-sm-8 field-value'}, result.error ? String(result.error) : String(result.value)));
     row.appendChild(
       DOM.div(
         { className: "col-sm-4 field p-2 border rounded-left" },
