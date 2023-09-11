788b694c662f2ae8e5493c2037e39d3d75d68f85
brmnl <50099074+brmnl@users.noreply.github.com>
2023-09-11 16:33:51 +0200

    Updating UI for operational status

--- src/dapp/index.js
+++ src/dapp/index.js
@@ -1,6 +1,7 @@
 import DOM from "./dom";
 import Contract from "./contract";
 import "./flightsurety.css";
+import TestAddreses from "./addresses.json";
 
 (async () => {
   let result = null;
@@ -9,9 +10,10 @@ import "./flightsurety.css";
     // Read transaction
     contract.isOperational((error, result) => {
       console.log(error, result);
-      display("Operational Status", "Check if contract is operational", [
-        { label: "Operational Status", error: error, value: result },
-      ]);
+      display(
+        [{ label: "Operational Status", error: error, value: result }],
+        "ops-status"
+      );
     });
 
     // User-submitted transaction
@@ -19,28 +21,32 @@ import "./flightsurety.css";
       let flight = DOM.elid("flight-number").value;
       // Write transaction
       contract.fetchFlightStatus(flight, (error, result) => {
-        display("Oracles", "Trigger oracles", [
-          {
-            label: "Fetch Flight Status",
-            error: error,
-            value: result.flight + " " + result.timestamp,
-          },
-        ]);
+        display_replace(
+          [
+            {
+              label: "Fetch Flight Status",
+              error: error,
+              value: result.flight + " " + result.timestamp,
+            },
+          ],
+          "display-wrapper"
+        );
       });
     });
   });
+  // Populate the options from the JSON file
+  populateAddressOptions("addroptions");
 })();
 
-function display(title, description, results) {
-  let displayDiv = DOM.elid("display-wrapper");
+function display(results, containerId) {
+  let displayDiv = DOM.elid(containerId);
   let section = DOM.section();
-  section.appendChild(DOM.h2(title));
-  section.appendChild(DOM.h5(description));
+
   results.map((result) => {
     let row = section.appendChild(DOM.div({ className: "row mt-4" }));
     row.appendChild(
       DOM.div(
-        { className: "col-sm-4 field p-2 border rounded-left" },
+        { className: "col-sm-4 field p-1 border rounded-left" },
         result.label
       )
     );
@@ -48,7 +54,7 @@ function display(title, description, results) {
       DOM.div(
         {
           className:
-            "col-sm-8 field-value p-2 border-top border-right border-bottom rounded-right",
+            "col-sm-8 field-value p-1 border-top border-right border-bottom rounded-right",
         },
         result.error ? String(result.error) : String(result.value)
       )
@@ -56,4 +62,54 @@ function display(title, description, results) {
     section.appendChild(row);
   });
   displayDiv.append(section);
+}
+
+function display_replace(results, containerId) {
+  let displayDiv = DOM.elid(containerId);
+  let section = displayDiv.querySelector("section");
+
+  if (!section) {
+    section = DOM.section();
+    displayDiv.appendChild(section);
+  } else {
+    while (section.firstChild) {
+      section.removeChild(section.firstChild);
+    }
+  }
+
+  results.map((result) => {
+    let row = section.appendChild(DOM.div({ className: "row mt-4" }));
+    row.appendChild(
+      DOM.div(
+        { className: "col-sm-4 field p-1 border rounded-left" },
+        result.label
+      )
+    );
+    row.appendChild(
+      DOM.div(
+        {
+          className:
+            "col-sm-8 field-value p-1 border-top border-right border-bottom rounded-right",
+        },
+        result.error ? String(result.error) : String(result.value)
+      )
+    );
+  });
+}
+
+function populateAddressOptions(className) {
+  try {
+    const selectElements = document.querySelectorAll(`.${className}`);
+
+    selectElements.forEach((selectElement) => {
+      TestAddreses.forEach((option) => {
+        const optionElement = document.createElement("option");
+        optionElement.value = option.value;
+        optionElement.textContent = option.label;
+        selectElement.appendChild(optionElement);
+      });
+    });
+  } catch (error) {
+    console.error("Error populating options:", error);
+  }
 }
